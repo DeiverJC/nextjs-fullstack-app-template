2be8b15b96980c393bd87e68ef1eb15f2f8e10e5
DeiverJC <deiver-jc@live.com>
2022-05-22 00:50:40 -0500

    feat: add CatCard using the component template

---
+++ components/cards/cat/CatCard.mocks.ts
@@ -0,0 +1,13 @@
+import { ICatCard } from './CatCard';
+
+const base: ICatCard = {
+  tag: 'Felines',
+  title: `What's new in Cats`,
+  body: 'Lorem pariatur sit eu cupidatat aliquip duis occaecat consectetur nostrud nostrud et Lorem culpa fugiat.',
+  author: 'Alex',
+  time: '2h ago',
+};
+
+export const mockCatCardProps = {
+  base,
+};
